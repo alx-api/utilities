aee3c792ec37e23148f487540543180626365e08
Edgar Caudillo <ialexk@gmail.com>
2026-08-11 14:59:44 -0600

    insight gaming v3

--- overlays/js/leaderboard.js
+++ overlays/js/leaderboard.js
@@ -51,20 +51,47 @@ function createRow(team, index){
 }
 
 function renderLeaderboard(data){
+    let sliceSize=0;
+    let leftColumn;
+    let centerColumn;
+    let rightColumn;
 
-    const leftColumn = document.getElementById("left-column");
-    const rightColumn = document.getElementById("right-column");
+    if (data.length > 16 ){
+        leftColumn = document.getElementById("column-1");
+        centerColumn = document.getElementById("column-2");
+        rightColumn = document.getElementById("column-3");
+        sliceSize = data.length / 6;
+    } else {
+        leftColumn = document.getElementById("left-column");
+        rightColumn = document.getElementById("right-column");
+        sliceSize = data.length / 2;
+    }
 
     document.querySelector(".title").innerHTML =
     `${event_name} • MAPA ${map_count}`;
 
-    const leftSide = data.slice(0,8);
-
-    const rightSide = data.slice(8,16);
-
-    leftColumn.innerHTML =
+    if (data.length > 16 ){
+        const leftSide = data.slice(0,sliceSize);
+        const centerSide = data.slice(sliceSize,sliceSize*2);        
+        const rightSide = data.slice(sliceSize*2,sliceSize * 3);
+        
+        leftColumn.innerHTML =
         leftSide.map((team, index) => createRow(team, index)).join("");
-
-    rightColumn.innerHTML =
+        
+        centerColumn.innerHTML =
+        centerSide.map((team, index) => createRow(team, index + 8)).join("");
+        
+        rightColumn.innerHTML =
+        rightSide.map((team, index) => createRow(team, index + 16)).join("");
+    } else {
+        const leftSide = data.slice(0,sliceSize);
+        
+        const rightSide = data.slice(sliceSize,sliceSize * 2);
+        
+        leftColumn.innerHTML =
+        leftSide.map((team, index) => createRow(team, index)).join("");
+        
+        rightColumn.innerHTML =
         rightSide.map((team, index) => createRow(team, index + 8)).join("");
+    }
 }
