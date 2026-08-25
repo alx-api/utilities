99c93022500714a9e844fd785bc01fa0e1541a22
Edgar Caudillo <ialexk@gmail.com>
2026-08-25 14:54:30 -0600

    generic v1

--- overlays/js/leaderboard.js
+++ overlays/js/leaderboard.js
@@ -7,7 +7,10 @@ let leaderboardData = [];
 let leaderboardPage = 0;
 
 function createRow(team, index){
-    const qualification = Number(team.points) >= 150 ? "matchpoint" : "";
+    const qualification = qualificationType || "matchpoint";
+    if(qualification == "survivor"){
+
+    }
     //const qualification = (index < 24) ? "qualified":"";
     const wiped = team.squads_alive <= 1
         ? "nogame"
